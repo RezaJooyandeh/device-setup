4c984a7d89f3255c06099e516a2babd9aa5ecc05
Reza Jooyandeh <mr.jooyandeh@gmail.com>
2021-06-25 09:56:32 -0700

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -11,7 +11,7 @@ Set-PoshPrompt -Theme morphism # Set custom theme for OhMyPosh. Run First-Time-S
 
 function gfa { git fetch --all -p }
 
-function gpa { git checkout master; git pull --prune; git branch -D $(git branch --merged).trim(); }
+function gpp { git pull --prune }
 
 function Complete-FirstTime-Setup {
 	function Install-PoshGit {
